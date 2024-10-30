7d99aeaa20f5d5c375fbc9f70684caf03fce6625
Manash <manashbaruah219@gmail.com>
2024-10-31 00:26:11 +0530

    update

--- js/countrynews.js
+++ js/countrynews.js
@@ -1,3 +1,23 @@
+// country search box
+
+const countrySearchInput = document.getElementById('country_search_input');
+const countrySearchBtn = document.getElementById('country_search_btn');
+
+countrySearchBtn.addEventListener("click", () => {
+    const c_query = countrySearchInput.value;
+    if (!c_query){
+        return
+    }
+    fetchNews(c_query);
+    selectedClickItem?.classList.remove('active');
+    selectedClickItem = null;
+})
+
+
+
+
+
+
 // const Trending_API_key = "pub_577574f79679ff0176758cf266b73293fcded" ;
 // const Trending_API_url = "https://newsdata.io/api/1/latest?apikey=";
 
@@ -9,25 +29,16 @@
 //     bindData(data.articles);
 // }
 
-async function fetchCountryNews(query2) {
-    const res = await fetch(`${API_url}${API_key}&country=${query2}`)
-    const data = await res.json();
-    console.log(data);
-    bindData(data.articles);
-}
+// const API_key = "bb280776c89d4d5abd3d4b68bcc20b7d" ;
+// const API_url = "https://newsapi.org/v2/everything?sortBy=publishedAt&q=";
 
-// country search box
-const countrySearchInput = document.getElementById('country_search_input');
-const countrySearchBtn = document.getElementById('country_search_btn');
+// async function fetchCountryNews(query2) {
+//     const API_key = "bb280776c89d4d5abd3d4b68bcc20b7d" ;
+//     const API_url = "https://newsapi.org/v2/everything?sortBy=publishedAt&q=";
+//     const res = await fetch(`${API_url}${API_key}&country=${query2}`)
+//     const data = await res.json();
+//     console.log(data);
+//     bindData(data.articles);
+// }
 
-countrySearchBtn.addEventListener("click", () => {
-    const c_query = countrySearchInput.value;
-    if (!c_query){
-        return
-    }
-    fetchCountryNews(c_query);
-    selectedClickItem?.classList.remove('active');
-    selectedClickItem = null;
-})
 
-// https://newsdata.io/api/1/news?apikey=pub_577574f79679ff0176758cf266b73293fcded&q=trending&country=al 
--- js/home.js
+++ js/home.js
@@ -33,3 +33,16 @@ contSearchBtn.addEventListener("click", () => {
     selectedClickItem = null;
 })
 
+// country search box
+const countrySearchInput = document.getElementById('country_search_input');
+const countrySearchBtn = document.getElementById('country_search_btn');
+
+countrySearchBtn.addEventListener("click", () => {
+    const c_query = countrySearchInput.value;
+    if (!c_query){
+        return
+    }
+    fetchNews(c_query);
+    selectedClickItem?.classList.remove('active');
+    selectedClickItem = null;
+})
